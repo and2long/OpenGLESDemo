13fbaf0e85e4251e490f9545d4b0e844611090b7
Lilong Zhang <and2long@gmail.com>
2023-02-03 14:27:45 +0800

    first commit.

---
+++ app/src/main/java/com/example/openglesdemo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.openglesdemo
+
+import android.opengl.GLSurfaceView
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var gLView: GLSurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+    }
+}
